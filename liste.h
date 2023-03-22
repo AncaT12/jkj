0816e1794f8813d897f119a0c172211558d5c0f2
AncaT12 <anca.teo12@gmail.com>
2023-03-22 13:04:58 +0200

    PALAB4

--- liste.h
+++ liste.h
@@ -1,5 +1,3 @@
-#ifndef LISTE_H_INCLUDED
-#define LISTE_H_INCLUDED
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +9,4 @@ typedef struct Node{
 void addAtBeginning(Node **head, Data v);
 void addAtEnd(Node** head, Data v);
 void display(Node *head);
-void modify(struct Node *head, int k, Data newVal);
 
-
-#endif // LISTE_H_INCLUDED
--- main.c
+++ main.c
@@ -1,51 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "liste.h"
 
-typedef int Data;
-
-
-
-void addAtBeginning(Node **head, Data v)
+int main()
 {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->val = v;
-	newNode->next = *head;
-	*head = newNode;
-}
+    struct Node *head = NULL;
 
-void addAtEnd(Node** head, Data v)
-{
-	Node *aux=*head;
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->val = v;
-	if (*head == NULL) addAtBeginning(&*head, v);
-	else{
-		while (aux->next!=NULL) aux = aux->next;
-		aux->next = newNode;
-		newNode->next = NULL;
-	}
-}
+    addAtEnd(&head, 1);
+    addAtEnd(&head, 2);
+    addAtEnd(&head, 3);
+    printf("Afisare dupa adaugarea la sfarsit: ");
+    display(head);
 
-void display(Node *head)
-{
-	while (head!=NULL){
-		printf ("%d ", head->val);
-		head=head->next;
-	}
-	printf("\n");
-}
-void modify(struct Node *head, int k, Data newVal)
-{
-    struct Node *current = head;
-    int i = 0;
+    addAtBeginning(&head, 0);
+    printf("Afisare dupa adaugarea la inceput: ");
+    display(head);
 
-    while (current != NULL && i < k) {
-        current = current->next;
-        i++;
-    }
 
-    if (current != NULL) {
-        current->val = newVal;
-    }
+    return 0;
 }
